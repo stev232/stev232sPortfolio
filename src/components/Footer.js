a39486af46f837ecfe3b00cb0e72d8aea6382c48
Stephen Merki <stephen.merki232@gmail.com>
2023-04-27 14:53:46 -0500

    Improved styles for the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,19 @@ const styles = {
   footerStyle: {
     background: '#00b3b3',
   },
-  headingStyle: {
-    fontSize: '20px',
-  },
 };
 
 function Footer() {
   return (
     <footer style={styles.footerStyle} className="footer">
-      <p style={styles.headingStyle}>
-        <strong>GitHub</strong>: <a href="https://github.com/stev232">stev232</a>
-        <strong>Linkedin</strong>: <a href="https://www.linkedin.com/in/stephen-merki-63064419a">Stephen Merki</a>
-        <strong>Twitter</strong>: <a href="https://twitter.com/Stev2332">stev2332</a>
+      <p>
+        <strong>GitHub</strong>: <a href="https://github.com/stev232">stev232</a> 
+      </p>
+      <p>
+        <strong>Linkedin</strong>: <a href="https://www.linkedin.com/in/stephen-merki-63064419a">Stephen Merki</a> 
+      </p>
+      <p>
+        <strong>Twitter</strong>: <a href="https://twitter.com/Stev2332">stev2332</a> 
       </p>
     </footer>
   );
